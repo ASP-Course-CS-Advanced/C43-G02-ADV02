fc5459134bbd1e63b9b424cd3a4bfbf43c1336c7
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-02-05 02:16:50 +0200

    Q03 - Given a Queue, implement a function to reverse the elements of a queue using a stack.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -37,6 +37,24 @@
 
         #endregion
 
+        #region Q03 - Given a Queue, implement a function to reverse the elements of a queue using a stack.
+
+        static Queue<int> ReverseQueue(Queue<int> queue)
+        {
+            Stack<int> stack = new Stack<int>();
+            while (queue.Count > 0)
+            {
+                stack.Push(queue.Dequeue());
+            }
+            while (stack.Count > 0)
+            {
+                queue.Enqueue(stack.Pop());
+            }
+            return queue;
+        }
+
+        #endregion
+
         static void Main(string[] args)
         {
 
@@ -80,6 +98,23 @@
 
             #endregion
 
+            #region Q03 - Given a Queue, implement a function to reverse the elements of a queue using a stack.
+
+            //Queue<int> queue = new Queue<int>();
+            //queue.Enqueue(1);
+            //queue.Enqueue(2);
+            //queue.Enqueue(3);
+            //queue.Enqueue(4);
+            //queue.Enqueue(5);
+
+            //queue = ReverseQueue(queue);
+
+            //foreach (int item in queue)
+            //{
+            //    Console.Write(item + " ");
+            //}
+
+            #endregion
         }
     }
 }
